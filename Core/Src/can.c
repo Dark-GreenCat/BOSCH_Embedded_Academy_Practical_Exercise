4d9a39869c44f312b9cf67e56c765f9597a84001
Dark-GreenCat <tranquanghuy3008@outlook.com>
2024-08-26 06:38:47 +0700

    [REFACTOR] Add .clang-format and format some files

--- Core/Src/can.c
+++ Core/Src/can.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    can.c
-  * @brief   This file provides code for the configuration
-  *          of the CAN instances.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    can.c
+ * @brief   This file provides code for the configuration
+ *          of the CAN instances.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "can.h"
@@ -50,14 +50,12 @@ void MX_CAN1_Init(void)
   hcan1.Init.AutoRetransmission = DISABLE;
   hcan1.Init.ReceiveFifoLocked = DISABLE;
   hcan1.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan1) != HAL_OK)
-  {
+  if (HAL_CAN_Init(&hcan1) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN CAN1_Init 2 */
 
   /* USER CODE END CAN1_Init 2 */
-
 }
 /* CAN2 init function */
 void MX_CAN2_Init(void)
@@ -82,30 +80,27 @@ void MX_CAN2_Init(void)
   hcan2.Init.AutoRetransmission = DISABLE;
   hcan2.Init.ReceiveFifoLocked = DISABLE;
   hcan2.Init.TransmitFifoPriority = DISABLE;
-  if (HAL_CAN_Init(&hcan2) != HAL_OK)
-  {
+  if (HAL_CAN_Init(&hcan2) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN CAN2_Init 2 */
 
   /* USER CODE END CAN2_Init 2 */
-
 }
 
-static uint32_t HAL_RCC_CAN1_CLK_ENABLED=0;
+static uint32_t HAL_RCC_CAN1_CLK_ENABLED = 0;
 
 void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
 {
 
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(canHandle->Instance==CAN1)
-  {
-  /* USER CODE BEGIN CAN1_MspInit 0 */
+  GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+  if (canHandle->Instance == CAN1) {
+    /* USER CODE BEGIN CAN1_MspInit 0 */
 
-  /* USER CODE END CAN1_MspInit 0 */
+    /* USER CODE END CAN1_MspInit 0 */
     /* CAN1 clock enable */
     HAL_RCC_CAN1_CLK_ENABLED++;
-    if(HAL_RCC_CAN1_CLK_ENABLED==1){
+    if (HAL_RCC_CAN1_CLK_ENABLED == 1) {
       __HAL_RCC_CAN1_CLK_ENABLE();
     }
 
@@ -114,7 +109,7 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
     PA11     ------> CAN1_RX
     PA12     ------> CAN1_TX
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_11|GPIO_PIN_12;
+    GPIO_InitStruct.Pin = GPIO_PIN_11 | GPIO_PIN_12;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -124,19 +119,17 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
     /* CAN1 interrupt Init */
     HAL_NVIC_SetPriority(CAN1_RX0_IRQn, 2, 0);
     HAL_NVIC_EnableIRQ(CAN1_RX0_IRQn);
-  /* USER CODE BEGIN CAN1_MspInit 1 */
+    /* USER CODE BEGIN CAN1_MspInit 1 */
 
-  /* USER CODE END CAN1_MspInit 1 */
-  }
-  else if(canHandle->Instance==CAN2)
-  {
-  /* USER CODE BEGIN CAN2_MspInit 0 */
+    /* USER CODE END CAN1_MspInit 1 */
+  } else if (canHandle->Instance == CAN2) {
+    /* USER CODE BEGIN CAN2_MspInit 0 */
 
-  /* USER CODE END CAN2_MspInit 0 */
+    /* USER CODE END CAN2_MspInit 0 */
     /* CAN2 clock enable */
     __HAL_RCC_CAN2_CLK_ENABLE();
     HAL_RCC_CAN1_CLK_ENABLED++;
-    if(HAL_RCC_CAN1_CLK_ENABLED==1){
+    if (HAL_RCC_CAN1_CLK_ENABLED == 1) {
       __HAL_RCC_CAN1_CLK_ENABLE();
     }
 
@@ -145,7 +138,7 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
     PB5     ------> CAN2_RX
     PB6     ------> CAN2_TX
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6;
+    GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_6;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
@@ -155,23 +148,22 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
     /* CAN2 interrupt Init */
     HAL_NVIC_SetPriority(CAN2_RX0_IRQn, 2, 0);
     HAL_NVIC_EnableIRQ(CAN2_RX0_IRQn);
-  /* USER CODE BEGIN CAN2_MspInit 1 */
+    /* USER CODE BEGIN CAN2_MspInit 1 */
 
-  /* USER CODE END CAN2_MspInit 1 */
+    /* USER CODE END CAN2_MspInit 1 */
   }
 }
 
 void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
 {
 
-  if(canHandle->Instance==CAN1)
-  {
-  /* USER CODE BEGIN CAN1_MspDeInit 0 */
+  if (canHandle->Instance == CAN1) {
+    /* USER CODE BEGIN CAN1_MspDeInit 0 */
 
-  /* USER CODE END CAN1_MspDeInit 0 */
+    /* USER CODE END CAN1_MspDeInit 0 */
     /* Peripheral clock disable */
     HAL_RCC_CAN1_CLK_ENABLED--;
-    if(HAL_RCC_CAN1_CLK_ENABLED==0){
+    if (HAL_RCC_CAN1_CLK_ENABLED == 0) {
       __HAL_RCC_CAN1_CLK_DISABLE();
     }
 
@@ -179,23 +171,21 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
     PA11     ------> CAN1_RX
     PA12     ------> CAN1_TX
     */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11 | GPIO_PIN_12);
 
     /* CAN1 interrupt Deinit */
     HAL_NVIC_DisableIRQ(CAN1_RX0_IRQn);
-  /* USER CODE BEGIN CAN1_MspDeInit 1 */
+    /* USER CODE BEGIN CAN1_MspDeInit 1 */
 
-  /* USER CODE END CAN1_MspDeInit 1 */
-  }
-  else if(canHandle->Instance==CAN2)
-  {
-  /* USER CODE BEGIN CAN2_MspDeInit 0 */
+    /* USER CODE END CAN1_MspDeInit 1 */
+  } else if (canHandle->Instance == CAN2) {
+    /* USER CODE BEGIN CAN2_MspDeInit 0 */
 
-  /* USER CODE END CAN2_MspDeInit 0 */
+    /* USER CODE END CAN2_MspDeInit 0 */
     /* Peripheral clock disable */
     __HAL_RCC_CAN2_CLK_DISABLE();
     HAL_RCC_CAN1_CLK_ENABLED--;
-    if(HAL_RCC_CAN1_CLK_ENABLED==0){
+    if (HAL_RCC_CAN1_CLK_ENABLED == 0) {
       __HAL_RCC_CAN1_CLK_DISABLE();
     }
 
@@ -203,84 +193,88 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
     PB5     ------> CAN2_RX
     PB6     ------> CAN2_TX
     */
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_5|GPIO_PIN_6);
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_5 | GPIO_PIN_6);
 
     /* CAN2 interrupt Deinit */
     HAL_NVIC_DisableIRQ(CAN2_RX0_IRQn);
-  /* USER CODE BEGIN CAN2_MspDeInit 1 */
+    /* USER CODE BEGIN CAN2_MspDeInit 1 */
 
-  /* USER CODE END CAN2_MspDeInit 1 */
+    /* USER CODE END CAN2_MspDeInit 1 */
   }
 }
 
 /* USER CODE BEGIN 1 */
 void MX_CAN1_Setup()
 {
-	HAL_CAN_ConfigFilter(&hcan1, &CAN1_sFilterConfig);
-	HAL_CAN_Start(&hcan1);
-	HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
+  HAL_CAN_ConfigFilter(&hcan1, &CAN1_sFilterConfig);
+  HAL_CAN_Start(&hcan1);
+  HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
 }
 
 void MX_CAN2_Setup()
 {
-	HAL_CAN_ConfigFilter(&hcan2, &CAN2_sFilterConfig);
-	HAL_CAN_Start(&hcan2);
-	HAL_CAN_ActivateNotification(&hcan2, CAN_IT_RX_FIFO0_MSG_PENDING);
+  HAL_CAN_ConfigFilter(&hcan2, &CAN2_sFilterConfig);
+  HAL_CAN_Start(&hcan2);
+  HAL_CAN_ActivateNotification(&hcan2, CAN_IT_RX_FIFO0_MSG_PENDING);
 }
 
-void CAN1CommSetup() {
-	CAN1_pHeader.IDE = CAN_ID_STD;
-	CAN1_pHeader.StdId = 0x078;
-	CAN1_pHeader.RTR = CAN_RTR_DATA;
-	CAN1_pHeader.DLC = 8;
-	CAN1_sFilterConfig.FilterActivation = CAN_FILTER_ENABLE;
-	CAN1_sFilterConfig.FilterBank = 14;
-	CAN1_sFilterConfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
-	CAN1_sFilterConfig.FilterIdHigh = 0x0A2 << 5;
-	CAN1_sFilterConfig.FilterIdLow = 0;
-	CAN1_sFilterConfig.FilterMaskIdHigh = 0x0A2 << 5;
-	CAN1_sFilterConfig.FilterMaskIdLow = 0;
-	CAN1_sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
-	CAN1_sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
-	CAN1_sFilterConfig.SlaveStartFilterBank = 16;
-	HAL_CAN_ConfigFilter(&hcan1, &CAN1_sFilterConfig);
+void CAN1CommSetup()
+{
+  CAN1_pHeader.IDE = CAN_ID_STD;
+  CAN1_pHeader.StdId = 0x078;
+  CAN1_pHeader.RTR = CAN_RTR_DATA;
+  CAN1_pHeader.DLC = 8;
+  CAN1_sFilterConfig.FilterActivation = CAN_FILTER_ENABLE;
+  CAN1_sFilterConfig.FilterBank = 14;
+  CAN1_sFilterConfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+  CAN1_sFilterConfig.FilterIdHigh = 0x0A2 << 5;
+  CAN1_sFilterConfig.FilterIdLow = 0;
+  CAN1_sFilterConfig.FilterMaskIdHigh = 0x0A2 << 5;
+  CAN1_sFilterConfig.FilterMaskIdLow = 0;
+  CAN1_sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
+  CAN1_sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
+  CAN1_sFilterConfig.SlaveStartFilterBank = 16;
+  HAL_CAN_ConfigFilter(&hcan1, &CAN1_sFilterConfig);
 }
 
-void CAN2CommSetup() {
-	CAN2_pHeader.IDE = CAN_ID_STD;
-	CAN2_pHeader.StdId = 0x0A2;
-	CAN2_pHeader.RTR = CAN_RTR_DATA;
-	CAN2_pHeader.DLC = 8;
-	CAN2_sFilterConfig.FilterActivation = CAN_FILTER_ENABLE;
-	CAN2_sFilterConfig.FilterBank = 18;
-	CAN2_sFilterConfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
-	CAN2_sFilterConfig.FilterIdHigh = CAN1_pHeader.StdId << 5;
-	CAN2_sFilterConfig.FilterIdLow = 0;
-	CAN2_sFilterConfig.FilterMaskIdHigh = CAN1_pHeader.StdId << 5;
-	CAN2_sFilterConfig.FilterMaskIdLow = 0;
-	CAN2_sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
-	CAN2_sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
-	HAL_CAN_ConfigFilter(&hcan2, &CAN2_sFilterConfig);
+void CAN2CommSetup()
+{
+  CAN2_pHeader.IDE = CAN_ID_STD;
+  CAN2_pHeader.StdId = 0x0A2;
+  CAN2_pHeader.RTR = CAN_RTR_DATA;
+  CAN2_pHeader.DLC = 8;
+  CAN2_sFilterConfig.FilterActivation = CAN_FILTER_ENABLE;
+  CAN2_sFilterConfig.FilterBank = 18;
+  CAN2_sFilterConfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+  CAN2_sFilterConfig.FilterIdHigh = CAN1_pHeader.StdId << 5;
+  CAN2_sFilterConfig.FilterIdLow = 0;
+  CAN2_sFilterConfig.FilterMaskIdHigh = CAN1_pHeader.StdId << 5;
+  CAN2_sFilterConfig.FilterMaskIdLow = 0;
+  CAN2_sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
+  CAN2_sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
+  HAL_CAN_ConfigFilter(&hcan2, &CAN2_sFilterConfig);
 }
 
-void read_from_buffer(uint8_t *req_buffer, uint16_t len, uint8_t *data_tx) {
-	data_tx[0] = len;
-	uint8_t i;
-	for (i = 0; i < len; i++) {
-		data_tx[i + 1] = req_buffer[i];
-	}
-	while (i <= 7) {
-		i++;
-		data_tx[i] = 0x55;
-	}
+void read_from_buffer(uint8_t* req_buffer, uint16_t len, uint8_t* data_tx)
+{
+  data_tx[0] = len;
+  uint8_t i;
+  for (i = 0; i < len; i++) {
+    data_tx[i + 1] = req_buffer[i];
+  }
+  while (i <= 7) {
+    i++;
+    data_tx[i] = 0x55;
+  }
 }
 
-void CAN1_Send() {
-	PrintCANLog(CAN1_pHeader.StdId, CAN1_DATA_TX);
-	if (HAL_CAN_AddTxMessage(&hcan1, &CAN1_pHeader, CAN1_DATA_TX,
-			&CAN1_pTxMailbox) != HAL_OK) {
-		Error_Handler();
-	}
-
+void CAN1_Send()
+{
+  PrintCANLog(CAN1_pHeader.StdId, CAN1_DATA_TX);
+  if (HAL_CAN_AddTxMessage(&hcan1, &CAN1_pHeader, CAN1_DATA_TX,
+          &CAN1_pTxMailbox)
+      != HAL_OK) {
+    Error_Handler();
+  }
 }
 /* USER CODE END 1 */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,26 +1,26 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "can.h"
-#include "usart.h"
 #include "gpio.h"
+#include "usart.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -57,26 +57,26 @@ uint32_t CAN1_pTxMailbox;
 uint32_t CAN2_pTxMailbox;
 
 uint16_t NumBytesReq = 0;
-uint8_t  REQ_BUFFER  [4096];
-uint8_t  REQ_1BYTE_DATA;
+uint8_t REQ_BUFFER[4096];
+uint8_t REQ_1BYTE_DATA;
 
-uint8_t CAN1_DATA_TX[8] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-uint8_t CAN1_DATA_RX[8] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-uint8_t CAN2_DATA_TX[8] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-uint8_t CAN2_DATA_RX[8] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+uint8_t CAN1_DATA_TX[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t CAN1_DATA_RX[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t CAN2_DATA_TX[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t CAN2_DATA_RX[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 uint16_t Num_Consecutive_Tester;
-uint8_t  Flg_Consecutive = 0;
+uint8_t Flg_Consecutive = 0;
 
 unsigned int TimeStamp;
 // maximum characters send out via UART is 30
-char bufsend[30]="XXX: D1 D2 D3 D4 D5 D6 D7 D8  ";
+char bufsend[30] = "XXX: D1 D2 D3 D4 D5 D6 D7 D8  ";
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-void PrintCANLog(uint16_t CANID, uint8_t * CAN_Frame);
+void PrintCANLog(uint16_t CANID, uint8_t* CAN_Frame);
 void SID_22_Practice();
 void SID_2E_Practice();
 void SID_27_Practice();
@@ -89,15 +89,15 @@ void delay(uint16_t delay);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-	uint16_t i,j = 0;
-	uint16_t Consecutive_Cntr = 0;
+  uint16_t i, j = 0;
+  uint16_t Consecutive_Cntr = 0;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -123,7 +123,7 @@ int main(void)
   MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
   CAN1CommSetup();
-	CAN2CommSetup();
+  CAN2CommSetup();
   MX_CAN1_Setup();
   MX_CAN2_Setup();
   __HAL_UART_ENABLE_IT(&huart3, UART_IT_RXNE);
@@ -134,54 +134,54 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   // Example Function to print can message via uart
   PrintCANLog(CAN1_pHeader.StdId, &CAN1_DATA_TX[0]);
-  while (1)
-  {
+  while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-		if (NumBytesReq != 0) {
-			delay(100);
-			read_from_buffer(REQ_BUFFER, NumBytesReq, CAN1_DATA_TX);
-			CAN1_Send();
-			delay(100);
-			NumBytesReq = 0;
-		}
+    if (NumBytesReq != 0) {
+      delay(100);
+      read_from_buffer(REQ_BUFFER, NumBytesReq, CAN1_DATA_TX);
+      CAN1_Send();
+      delay(100);
+      NumBytesReq = 0;
+    }
 
-    if(!BtnU) /*IG OFF->ON stimulation*/
+    if (!BtnU) /*IG OFF->ON stimulation*/
     {
       delay(20);
-      USART3_SendString((uint8_t *)"IG OFF ");
-      while(!BtnU);
+      USART3_SendString((uint8_t*) "IG OFF ");
+      while (!BtnU)
+        ;
       MX_CAN1_Setup();
       MX_CAN2_Setup();
-      USART3_SendString((uint8_t *)"-> IG ON \n");
+      USART3_SendString((uint8_t*) "-> IG ON \n");
       delay(20);
     }
   }
 
-  memset(&REQ_BUFFER,0x00,4096);
+  memset(&REQ_BUFFER, 0x00, 4096);
   NumBytesReq = 0;
 
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -191,84 +191,82 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLN = 168;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+      | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
     Error_Handler();
   }
 }
 
 /* USER CODE BEGIN 4 */
-void PrintCANLog(uint16_t CANID, uint8_t *CAN_Frame) {
-	uint16_t loopIndx = 0;
-	char bufID[5] = "   ";
-	char bufDat[3] = "  ";
-	char bufTime[8] = "        ";
+void PrintCANLog(uint16_t CANID, uint8_t* CAN_Frame)
+{
+  uint16_t loopIndx = 0;
+  char bufID[5] = "   ";
+  char bufDat[3] = "  ";
+  char bufTime[8] = "        ";
 
-	sprintf(bufTime, "%d", TimeStamp);
-	USART3_SendString((uint8_t*) bufTime);
-	USART3_SendString((uint8_t*) " ");
+  sprintf(bufTime, "%d", TimeStamp);
+  USART3_SendString((uint8_t*) bufTime);
+  USART3_SendString((uint8_t*) " ");
 
-	sprintf(bufID, "%03X", CANID);
-	for (loopIndx = 0; loopIndx < 3; loopIndx++) {
-		bufsend[loopIndx] = bufID[loopIndx];
-	}
-	bufsend[3] = ':';
-	bufsend[4] = ' ';
+  sprintf(bufID, "%03X", CANID);
+  for (loopIndx = 0; loopIndx < 3; loopIndx++) {
+    bufsend[loopIndx] = bufID[loopIndx];
+  }
+  bufsend[3] = ':';
+  bufsend[4] = ' ';
 
-	for (loopIndx = 0; loopIndx < 8; loopIndx++) {
-		sprintf(bufDat, "%02X", CAN_Frame[loopIndx]);
-		bufsend[loopIndx * 3 + 5] = bufDat[0];
-		bufsend[loopIndx * 3 + 6] = bufDat[1];
-		bufsend[loopIndx * 3 + 7] = ' ';
-	}
-	bufsend[29] = '\n';
-	USART3_SendString((unsigned char*) bufsend);
+  for (loopIndx = 0; loopIndx < 8; loopIndx++) {
+    sprintf(bufDat, "%02X", CAN_Frame[loopIndx]);
+    bufsend[loopIndx * 3 + 5] = bufDat[0];
+    bufsend[loopIndx * 3 + 6] = bufDat[1];
+    bufsend[loopIndx * 3 + 7] = ' ';
+  }
+  bufsend[29] = '\n';
+  USART3_SendString((unsigned char*) bufsend);
 }
 
 void delay(uint16_t delay)
 {
-	HAL_Delay(delay);
+  HAL_Delay(delay);
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t* file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
